b5112b416dc1853625923efc9be09413ce1aa342
tiagfernandes <tiago_fernandes@live.fr>
2017-03-06 11:44:52 +0100

    New*

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/facecast');
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://localhost:27017/facecast'; 
+mongoose.connect(mongoDB);
+var dba = mongoose.connection;
+dba.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var index = require('./routes/index');
-var event = require('./routes/event');
-var insertEvent = require('./routes/insertEvent');
+var events = require('./routes/events');
+var insert = require('./routes/insert');
+var android = require('./routes/android');
 
 var app = express();
 
@@ -28,14 +32,15 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
+/*app.use(function(req,res,next){
     req.db = db;
     next();
-});
+});*/
 
 app.use('/', index);
-app.use('/event', event);
-app.use('/insertEvent', insertEvent);
+app.use('/events', events);
+app.use('/insert', insert);
+app.use('/android', android);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
