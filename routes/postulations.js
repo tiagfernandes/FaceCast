5a5187560adedd15c3854663c9e847d3a279a559
Mickael Ducelier <Mickael Ducelier>
2017-03-21 16:17:26 +0100

    Postulation

--- routes/postulations.js
+++ routes/postulations.js
@@ -5,7 +5,7 @@ var postulation = require('../models/postulation');
 /* GET home page. */
 router.get('/', function(req, res, next) {    
     postulation.find({},{},function(e,docsPostulation){
-        res.render('postulations', {
+        res.render('postulations', { 
             "title" : "Liste des postulations",
             "postulationlist" : docsPostulation
         });
@@ -14,6 +14,23 @@ router.get('/', function(req, res, next) {
         .populate({path: '_offre', populate: {path: '_role' , model: 'role'} });
 });
 
+
+// Changer l'etat   
+
+router.post('/update/:id',function(req, res, next) {
+    id = req.params.id;
+    etat = req.body.etat;
+    
+    postulation.findOneAndUpdate({"_id": id},{ $set:{ "etat": etat} }, function(e,docs){
+        if(e){
+            res.status(500).send(e);  
+        }
+        else{       
+            // Redirection vers la liste
+            res.redirect("/postulations");
+        }
+    });
+});
 
 
 module.exports = router;
